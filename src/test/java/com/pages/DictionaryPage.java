efc828c9150cb8c208d77938c2f8f4d9e608f261
paulaklein20 <paulaklein20@gmail.com>
2016-07-13 15:11:10 +0300

    Update DictionaryPage.java
    enter_keywords is modified: enter_keywords_modified_by_another_user_github

--- src/test/java/com/pages/DictionaryPage.java
+++ src/test/java/com/pages/DictionaryPage.java
@@ -22,7 +22,7 @@ public class DictionaryPage extends PageObject {
     @FindBy(name="go")
     private WebElementFacade lookupButton;
 
-    public void enter_keywords(String keyword) {
+    public void enter_keywords_modified_by_another_user_github(String keyword) {
         searchTerms.type(keyword);
     }
 
