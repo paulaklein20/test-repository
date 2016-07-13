05a7de035890c05cd79a65dca4d720e883b24532
paula <paulaklein20@gmail.com>
2016-07-13 15:13:27 +0300

    modificare enter_keywords

--- src/test/java/com/pages/DictionaryPage.java
+++ src/test/java/com/pages/DictionaryPage.java
@@ -22,7 +22,7 @@ public class DictionaryPage extends PageObject {
     @FindBy(name="go")
     private WebElementFacade lookupButton;
 
-    public void enter_keywords(String keyword) {
+    public void enter_keywords_modified_by_me_local(String keyword) {
         searchTerms.type(keyword);
     }
 
--- src/test/java/com/steps/serenity/EndUserSteps.java
+++ src/test/java/com/steps/serenity/EndUserSteps.java
@@ -14,7 +14,7 @@ public class EndUserSteps extends ScenarioSteps {
 
     @Step
     public void enters(String keyword) {
-        dictionaryPage.enter_keywords(keyword);
+        dictionaryPage.enter_keywords_modified_by_me_local(keyword);
     }
 
     @Step
